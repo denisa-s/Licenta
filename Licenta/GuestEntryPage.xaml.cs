555d90b9b20031f313de48eacaedbbf51d7a37c4
Stoica Denisa <teodora202@yahoo.com>
2023-04-28 23:25:35 +0300

    login

--- Licenta/GuestEntryPage.xaml.cs
+++ Licenta/GuestEntryPage.xaml.cs
@@ -1,18 +1,21 @@
 using Licenta.Models;
+using System.Security.Cryptography;
 
 namespace Licenta;
 
 public partial class GuestEntryPage : ContentPage
 {
+    public string email { get; set; }
     public GuestEntryPage()
-	{
-		InitializeComponent();
-	}
-    /*protected override async void OnAppearing()
     {
-        base.OnAppearing();
-        listView.ItemsSource = await App.Database.GetGuestsAsync();
-    }*/
+        InitializeComponent();
+        email = App.GlobalObject.UserName;
+        var loginData = App.Database.RetrieveDataFromDatabase(email);
+        LoginModel result = loginData.Result;
+        emailLabel.Text = result.UserName;
+        passwordEntry.Text = result.Password;
+    }
+
     async void OnGuestAddedClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new GuestPage
@@ -45,17 +48,37 @@ public partial class GuestEntryPage : ContentPage
         });
 
     }
-    /*async void OnEditEmailClicked(object sender, EventArgs e)
+    async void OnChangePasswordClicked(object sender, EventArgs e)
     {
-
-        var loginData = await App.Database.GetLoginDataAsync(UserName);
-        if (loginData != null)
+        email = App.GlobalObject.UserName;
+        var loginData = App.Database.RetrieveDataFromDatabase(email);
+        LoginModel result = loginData.Result;
+        result.Password = HashPassword(passwordEntry.Text);
+        await App.Database.SaveUserDataAsync(result);
+        await Navigation.PushAsync(new GuestEntryPage());
+    }
+    public static string HashPassword(string password)
+    {
+        byte[] salt = new byte[16];
+        using (var rng = RandomNumberGenerator.Create())
         {
-            if (string.Equals(loginData.UserName, UserName))
-            {
-                
-            }
-            
+            rng.GetBytes(salt);
         }
-    }*/
+
+        byte[] hash = HashPasswordWithSalt(password, salt);
+
+        var combinedSaltAndHash = new byte[salt.Length + hash.Length];
+        Array.Copy(salt, 0, combinedSaltAndHash, 0, salt.Length);
+        Array.Copy(hash, 0, combinedSaltAndHash, salt.Length, hash.Length);
+
+        return Convert.ToBase64String(combinedSaltAndHash);
+    }
+    private static byte[] HashPasswordWithSalt(string password, byte[] salt)
+    {
+        const int iterations = 10000;
+        using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations))
+        {
+            return pbkdf2.GetBytes(20); // 20 bytes for SHA1
+        }
+    }
 }
