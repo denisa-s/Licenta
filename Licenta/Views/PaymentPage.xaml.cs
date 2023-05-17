02ce8db030c2157808d2224388a2811113c34d8a
Stoica Denisa <teodora202@yahoo.com>
2023-05-17 12:28:45 +0300

    payment page

--- Licenta/Views/PaymentPage.xaml.cs
+++ Licenta/Views/PaymentPage.xaml.cs
@@ -7,14 +7,38 @@ public partial class PaymentPage : ContentPage
 	public PaymentPage()
 	{
 		InitializeComponent();
-	}
+        List<DateTime> dates = new List<DateTime>();
+        for (int i = 0; i < 300; i++)
+        {
+            dates.Add(DateTime.Today.AddMonths(i));
+        }
+
+        // Add the dates to the picker
+        foreach (DateTime date in dates)
+        {
+            DatePicker.Items.Add(date.ToString("MM/yyyy"));
+        }
+    }
+    private void OnDateSelectedIndexChanged(object sender, EventArgs e)
+    {
+        // Retrieve the selected date from the picker
+        string selectedDate = DatePicker.SelectedItem as string;
+        if (selectedDate != null)
+        {
+            DateTime dateValue = DateTime.ParseExact(selectedDate, "MM/yyyy", null);
+
+            // Use the selected date as needed
+            // For example, display it in a label
+            DisplayLabel.Text = dateValue.ToString("MM/yyyy");
+        }
+    }
     private async void OnFinishedClicked(object sender, EventArgs e)
 	{
         var card = new CardDetail
         {
             CardNumber = CardNumberEntry.Text,
             CardHolder = CardHolderEntry.Text,
-            ValidUntil = ValidUntilEntry.Text,
+            ValidUntil = DisplayLabel.Text,
             SecurityCode = SecurityCodeEntry.Text,
             Email = App.GlobalObject.UserName
     };
